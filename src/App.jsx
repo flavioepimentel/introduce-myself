1a041be29d3091605817b9be75ba527d17a54c19
flavioepimentel <flavio.ericksan@hotmail.com>
2022-05-03 23:04:35 -0300

    Corrige endereço, importa home e adiciona texto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header>
-
+        <img href='#' />
+        <ul>
+          <li>Instagram</li>
+          <li>Linkedin</li>
+          <li>Github</li>
+        </ul>
       </header>
+      <Home />
+
     </div>
+    </>
   );
 }
 
