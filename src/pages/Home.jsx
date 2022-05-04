6ed0be1709c37ef57929fbb7a1dee4fba744f82d
flavioepimentel <flavio.ericksan@hotmail.com>
2022-05-03 23:06:11 -0300

    Cria arquivo home com style

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Home extends React.Component {
+    render(){
+        return (
+            <main>
+                <h1>Flávio Pimentel</h1>
+                <h5>Desenvolvedor Full-Stack & DataScientist</h5>
+                <br/>
+                <p>Desenvolvendo soluções desde 2020, utilizo como principais 
+                    ferramentas Python, JavaScript, TypeScript, SQL e NoSQL,
+                    entre outras tecnologias e frameworks.
+                    </p>
+            </main>
+        )
+    }
+}
+
+export default Home
