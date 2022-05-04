c1cbe3fe299e5e9f79760abfd13ff9df4d31f2ec
flavioepimentel <flavio.ericksan@hotmail.com>
2022-05-03 23:35:14 -0300

    Implementa divisão de arquivo e seta

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ class Home extends React.Component {
     render(){
         return (
             <main>
+                <section>
+                    <div>
                 <h1>Flávio Pimentel</h1>
                 <h5>Desenvolvedor Full-Stack & DataScientist</h5>
                 <br/>
@@ -11,6 +13,11 @@ class Home extends React.Component {
                     ferramentas Python, JavaScript, TypeScript, SQL e NoSQL,
                     entre outras tecnologias e frameworks.
                     </p>
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon="fa-duotone fa-chevrons-down" />
+                    </div>
+                    </section>
             </main>
         )
     }
