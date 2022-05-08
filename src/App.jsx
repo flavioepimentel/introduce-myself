8a58bcd45b9bedc6a0daa7dbd2ac6d0578aed8eb
flavioepimentel <flavio.ericksan@hotmail.com>
2022-05-07 23:04:28 -0300

    Implementa video no background

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,73 @@
-import './style/App.css';
-import Home from './pages/Home';
+import "./style/App.css";
+import Home from "./pages/Home";
+const introvideo = require("./midia/introvideo.mp4");
 
 function App() {
   return (
     <>
-    <div className="App">
-      <header>
-        <img href='#' />
-        <ul>
-          <li>
-            <a target="_blank" href="https://wa.me/message/RMPN3E5VLSP4L1" rel="noreferrer"> WhatsApp</a> 
+      <div className="App">
+        <div className="video-containier">
+          <video
+            className="introvideo"
+            src={introvideo}
+            width="100%"
+            height="auto"
+            autoPlay
+            volume={0}
+            muted
+            loop
+            playsinline
+          />
+        </div>
+        <header>
+          <div className="links">
+            <ul className="links">
+              <li>
+                <a
+                  target="_blank"
+                  href="https://wa.me/message/RMPN3E5VLSP4L1"
+                  rel="noreferrer"
+                >
+                  WhatsApp
+                </a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  href="https://www.linkedin.com/in/flavioepimentel/"
+                  rel="noreferrer"
+                >
+                  Linkedin
+                </a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  href="https://github.com/flavioepimentel"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+              </li>
+            </ul>
+          </div>
+        </header>
+        <navbar>
+          <ul>
+            <li className="button">
+              <a target="_blank" href="#" rel="noreferrer">
+                WORKS
+              </a>
             </li>
-          <li>
-            <a target="_blank" href='https://www.linkedin.com/in/flavioepimentel/' rel="noreferrer">Linkedin</a>
+            <li className="button">
+              <a target="_blank" href="#" rel="noreferrer">
+                SOBRE
+              </a>
             </li>
-          <li> 
-            <a target="_blank" href='https://github.com/flavioepimentel' rel="noreferrer">Github</a>
-          </li>
-        </ul>
-      </header>
-      <Home />
-
-    </div>
+          </ul>
+        </navbar>
+        <Home />
+      </div>
     </>
   );
 }
