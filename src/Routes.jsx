c03bea5c0188b4dab40daf8eae6db53883671e52
flavioepimentel <flavio.ericksan@hotmail.com>
2022-05-08 19:00:08 -0300

    Implementa navegação e corrige sintaxe

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Works from "./pages/Works";
 import Sobre from "./pages/Sobre";
 
-
 function AppRoutes() {
   return (
-    <Routes>
-      <Route path={"/"} component={Home} exact />
-      <Route path="/works" component={Works} exact />
-      <Route path="/sobre" component={Sobre} exact />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="works" element={<Works />} exact />
+        <Route path="sobre" element={<Sobre />} exact />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
