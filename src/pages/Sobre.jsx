6c13b5fda3bdb9b19aff436db74b40cff83445ad
flavioepimentel <flavio.ericksan@hotmail.com>
2022-05-07 23:38:22 -0300

    Primiro commit

---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+class Sobre extends React.Component{
+  render() {
+      return(
+           <div />);
+}
+}
+export default Sobre
---
+++ src/pages/Works.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+class Works extends React.Component{
+  render() {
+      return(
+           <div />);
+}
+}
+export default Works
