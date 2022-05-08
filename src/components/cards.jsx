fb8b93d96820fb0aa15a3030c6abf36bd647f578
flavioepimentel <flavio.ericksan@hotmail.com>
2022-05-08 08:36:46 -0300

    Corrige formato inicial dos cards

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { View } from 'react-native';
 
 // import { Container } from './styles';
 
 const Card = () => {
-  return <View />;
+  return ;
 }
 
 export default Card;
