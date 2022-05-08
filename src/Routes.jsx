f0a3ceb0f279cbeb87ebbcd6447af4257015f97e
flavioepimentel <flavio.ericksan@hotmail.com>
2022-05-08 08:34:22 -0300

    Implementa rotas sobre e works

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Works from "./pages/Works";
+import Sobre from "./pages/Sobre";
+
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path={"/"} component={Home} exact></Route>
+      <Route path={"/"} component={Home} exact />
+      <Route path="/works" component={Works} exact />
+      <Route path="/sobre" component={Sobre} exact />
     </Routes>
   );
 }
