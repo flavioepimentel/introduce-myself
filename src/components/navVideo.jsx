334adbd604382409df257957094ec761caa9d00d
flavioepimentel <flavio.ericksan@hotmail.com>
2022-05-12 00:10:37 -0300

    Commit

--- src/components/navVideo.jsx
+++ src/components/navVideo.jsx
@@ -3,12 +3,11 @@ import { Link } from "react-router-dom";
 import "../style/Home.css";
 import Video from "./video";
 
-
 class NavVideo extends React.Component {
   render() {
     return (
       <>
-      <Video />
+        <Video />
         <header>
           <div className="links">
             <ul className="ul-links">
@@ -47,16 +46,12 @@ class NavVideo extends React.Component {
         </header>
         <nav className="navbar">
           <ul>
-            <li className="button">
-              <Link className="a-button" to={"/works"}>
-                WORKS
-              </Link>
-            </li>
-            <li className="button">
-              <Link className="a-button" to={"/sobre"}>
-                SOBRE
-              </Link>
-            </li>
+            <Link className="a-button" to={"/works"}>
+              <li className="button">WORKS</li>
+            </Link>
+            <Link className="a-button" to={"/sobre"}>
+              <li className="button">SOBRE</li>
+            </Link>
           </ul>
         </nav>
       </>
