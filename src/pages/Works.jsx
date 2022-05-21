89ab227a8a7e0d98ae8b0101d834268ad6c61f57
flavioepimentel <flavio.ericksan@hotmail.com>
2022-05-21 18:56:05 -0300

    Implementa cards com jobs

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -2,18 +2,27 @@ import React from "react";
 import "../style/Home.css";
 import "../style/Works.css";
 import Navbar from "../components/navbar";
-
+import { Link } from "react-router-dom";
 
 class Works extends React.Component {
   render() {
     return (
       <>
-      <Navbar/>
-      <main>
-        <div>
-          <p>Ola</p>
-        </div>
-      </main>
+        <Navbar />
+        <main>
+          <section>
+            <div className="card-1">
+              <img src="#" alt="#" />
+              <span href="http://americaleiloesconsultoria.com.br/">America Leilões</span>
+              <p>
+                Fui responsável por elaborar a coleta de dados, definir a
+                estrutura de armazenamento desses dados utilizando Python e
+                MongoDB, fui responsável por implementar API RestFull, regras de
+                acesso e filtros utilizando Node JS.
+              </p>
+            </div>
+          </section>
+        </main>
       </>
     );
   }
