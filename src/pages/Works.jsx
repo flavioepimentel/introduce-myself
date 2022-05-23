64b91f53426b3695f1fbcdd28408b9bd35a32ca6
flavioepimentel <flavio.ericksan@hotmail.com>
2022-05-22 23:02:09 -0300

    Tranforma card em componente

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -2,26 +2,19 @@ import React from "react";
 import "../style/Home.css";
 import "../style/Works.css";
 import Navbar from "../components/navbar";
-import { Link } from "react-router-dom";
+import Card from "../components/cards";
+
+
+
 
 class Works extends React.Component {
   render() {
     return (
       <>
         <Navbar />
-        <main>
-          <section>
-            <div className="card-1">
-              <img src="#" alt="#" />
-              <span href="http://americaleiloesconsultoria.com.br/">America Leilões</span>
-              <p>
-                Fui responsável por elaborar a coleta de dados, definir a
-                estrutura de armazenamento desses dados utilizando Python e
-                MongoDB, fui responsável por implementar API RestFull, regras de
-                acesso e filtros utilizando Node JS.
-              </p>
-            </div>
-          </section>
+        <main className="main-work">
+          {/* {it?.map()} */}
+          <Card />
         </main>
       </>
     );
